ad920fcc030d60057fddcf7d17b3b7cfa8a22343
Sean Altemose <sean@seanaltemose.com>
2019-04-25 02:47:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function keepGoing(array, changeValue, skipValue) {
 
 function findBy(array, findFn) {
   for (var i=0; i < array.length; i++) {
-    if (array[i] == findFn) {
+    if (findFn(array[i])) {
     break
   }
   }
