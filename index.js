ef1cc26d9dff372d0a6198aa6b341906f70721b2
Sean Altemose <sean@seanaltemose.com>
2019-04-25 02:35:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,13 @@ function breakOut(array, changeValue, stopValue){
 }
     return array
 }
+
+function keepGoing(array, changeValue, skipValue) {
+  for (var i=0; i < array.length, i++) {
+    if (array[i] == skipValue) {
+      continue;
+    }
+    else (array[i] = changeValue)
+  }
+  return array;
+}
